a29f45c3846617de69860b250253181803cd9bc0
143_3114006282_池文政 <526660788@qq.com>
2017-06-17 23:37:54 +0800

    适配分辨率初步

--- src/js/onsale.js
+++ src/js/onsale.js
@@ -1,10 +1,18 @@
 $(function(){
 	adjustgoodsimg();     //商品图片宽高比例调整
-
+	adjustgoodsinfo();
 });
 
 function adjustgoodsimg() {                   //商品图片宽高比例调整
 	var a=$('.goods-img').width();
 	a=a/3*4;
 	$('.goods-img').height(a);
+}
+
+function adjustgoodsinfo() {
+	var a= $('.goods').width();
+	a=a*0.59;
+	var b= $('.goods').height();
+	$('.goods-info').width(a);
+	$('.goods-info').height(b);
 }
