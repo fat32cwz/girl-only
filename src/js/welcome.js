973c93878ac2cc65c1b9f30a88d8a649776e215c
143_3114006282_池文政 <526660788@qq.com>
2017-06-18 20:27:19 +0800

    sweetalert和登录注销对接

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -38,11 +38,52 @@ function login() {
                  username : username,
                  password_hash: password_hash,
              },
-             success: function(){
-             	window.location.href = "index.html";
+             success: function(resp){
+             	if (resp.message=='success') {
+             		swal({
+                        title:'登录成功！',
+                        type: 'success',
+                        timer:2000,
+                        background:'#fff',
+                        confirmButtonColor: '#fc8c9c',
+                    });
+             		setTimeout("window.location = 'index.html'",2000); 
+             	}
+             	else if (resp.message=='参数不合法') {
+                    swal({
+                        text:'输入不正确，请重新输入!',
+                        title:'登录失败',
+                        type: 'warning',
+                        timer:2000,
+                        background:'#fff',
+                        confirmButtonColor: '#fc8c9c',
+                    });
+             	}
+             	else if (resp.message=='密码错误') {
+                    swal({
+                        text:'密码错误!',
+                        title:'登录失败',
+                        type: 'error',
+                        timer:2000,
+                        background:'#fff',
+                        confirmButtonColor: '#fc8c9c',
+                    });
+             	}
+             	else if (resp.message=='用户名不存在') {
+             		swal({
+                        text:'用户名不存在!',
+                        title:'登录失败',
+                        type: 'warning',
+                        timer:2000,
+                        background:'#fff',
+                        confirmButtonColor: '#fc8c9c',
+                    });
+             	}
              },
          });
 	}); 
 
 	
 }
+
+
